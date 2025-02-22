66690a73082020442e53308f05d092401aff0daf
shirocodes <ishirapt@gmail.com>
2025-02-22 03:14:05 +0300

    Created the operator functions and basic HTML structure

--- src/calc.js
+++ src/calc.js
@@ -1,12 +1,34 @@
-function add(number) {
-    return number + number;
+function add(number1, number2) {
+    return number1 + number2;
 }
-function substract(number) {
-    return number - number;
+function substract(number1, number2) {
+    return number1 - number2;
 }
-function mutiply(number) {
-    return number * number;
+function mutiply(number1, number2) {
+    return number1 * number2;
 }
-function divide(number) {
-    return number/number;
+function divide(number1, number2) {
+    if (number2 === 0) {
+        return "ERROR: Cannot divide by zero"
+    }
+    return number1/number2;
 }
+
+function operate(number1, number2, operator) {
+    switch (operator){
+        case "+":
+            return add(number1, number2);
+        case "-":
+            return substract(number1, number2);
+        case "*":
+            return mutiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+        default:
+            return "ERROR: Insert either +, -, *, /";
+    }
+
+}
+
+
+
